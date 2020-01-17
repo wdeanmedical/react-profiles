303db9711dda4dd67ba4e54c9e640f1ce9c83f0b
Nick Sophinos <nick@wdeanmedical.com>
2020-01-17 12:31:34 -0500

    added react-animation-frame

--- src/Card.js
+++ src/Card.js
@@ -1,11 +1,21 @@
 
 import React, { Component } from 'react';
+import ReactAnimationFrame from 'react-animation-frame'
 import './style.css';
 
 class Card extends Component {
 
   state = {
     editable: false,
+  }
+
+  onAnimationFrame(time) {
+    const progress = Math.round(time / this.props.durationMs * 100);
+    this.bar.style.width = `${progress}%`;
+
+    if (progress === 100) {
+      this.props.endAnimation();
+    }
   }
 
   handleEditClick = () => {
@@ -23,6 +33,7 @@ class Card extends Component {
     const { editable } = this.state;
     return (
       <div className="card">
+
         <div className="card-header">
           <img
             src="https://wdeanmedical.com/edit.png"
@@ -45,9 +56,13 @@ class Card extends Component {
         }
         <div className="body-text">{user.phone}</div>
         <div className="body-text city-country">{user.location.city}, {user.location.state}</div>
+        <div className="timer">
+          <p>{this.props.message}</p>
+          <div className="timer__bar" ref={node => this.bar = node}></div>
+        </div>
       </div>
     )
   }
 }
 
-export default Card;
+export default ReactAnimationFrame(Card, 10);
--- src/index.js
+++ src/index.js
@@ -42,6 +42,7 @@ class App extends Component {
             key={user.login.uuid}
             user={user}
             updateEmail={(email) => this.updateEmail(user.login.uuid, email)}
+            durationMs={1000}
           />
         )
       })
